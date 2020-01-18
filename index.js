63461d4909639716d9e548bd3968b0444bd69ccb
name <aya.ablaze@gmail.com>
2020-01-18 15:20:11 -0600

    playing around with css

---
+++ index.js
@@ -0,0 +1,13 @@
+const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+const height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
+
+function buildElement() {
+  let x = Math.random * width;
+  let y = Math.random * height;
+}
