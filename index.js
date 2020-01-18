3ce15328146f09bd5601167b17804aec77a99d81
name <aya.ablaze@gmail.com>
2020-01-18 17:28:16 -0600

    populate randomly placed circles

--- index.js
+++ index.js
@@ -7,7 +7,30 @@ const height =
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
-function buildElement() {
-  let x = Math.random * width;
-  let y = Math.random * height;
+let num = 0;
+
+function loadAtRandom() {
+  let timer = setInterval(function() {
+    makeCircle();
+    num++;
+
+    if (num > 500) clearInterval(timer);
+  }, 80);
+}
+
+function makeCircle() {
+  let circle = document.createElement("circle");
+  circle.id = "circle" + num;
+  let x = Math.floor(Math.random() * width);
+  let y = Math.floor(Math.random() * height);
+  let circleStyle = circle.style;
+  circleStyle.position = "absolute";
+  circleStyle.height = "22px";
+  circleStyle.width = "22px";
+  circleStyle.backgroundColor = "gold";
+  circleStyle.borderRadius = "50%";
+  circleStyle.top = y + "px";
+  circleStyle.left = x + "px";
+  //   circleStyle.opacity = 0.3;
+  document.getElementById("main").appendChild(circle);
 }
